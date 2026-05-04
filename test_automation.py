47d6c8237210140268afe398193e65e055798845
Leshanie Manusha <Leshaniebiz@gmail.com>
2026-05-04 16:00:08 +0530

    docs; updated readme

--- test_automation.py
+++ test_automation.py
@@ -10,10 +10,10 @@ from openpyxl.cell.cell import MergedCell
 
 # Configuration
 ROOT_DIR = Path(__file__).resolve().parent.parent
-TESTS_DIR = ROOT_DIR / "test_automation"
+TESTS_DIR = ROOT_DIR / "IT23157446_ITPM_test_automation"
 
 DEFAULT_EXCEL_CANDIDATES = [
-    str(TESTS_DIR / "Assignment 1 - Test cases.xlsx"),
+    str(TESTS_DIR / "IT23157446.xlsx"),
 ]
 
 DEFAULT_SHEET_NAME = " Test cases"
